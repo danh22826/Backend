4066dcae4089837321b8f30d3fe37f4070cbd555
NGUYEN DUC ANH <nguyenducanh228k6@gmail.com>
2026-04-06 03:18:07 +0700

    fwfw

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "Backend is running!";
     }
